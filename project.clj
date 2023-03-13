e3a06689d4538013a74c37ad99bcfeb73fe03eed
Bill Hedworth <bill.hedworth@gmail.com>
2023-03-13 09:58:21 +0000

    new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bigsy/dynamo-embedded-clj "0.1.1"
+(defproject org.clojars.bigsy/dynamo-embedded-clj "0.1.2"
   :description "Embedded dynamodb for clojure"
   :url "https://github.com/Bigsy/dynamo-embedded-clj"
   :license {:name "Eclipse Public License"
