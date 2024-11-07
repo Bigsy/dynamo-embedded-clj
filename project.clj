36f02cebe009fe12c8ec35b5058290df02209588
Bill Hedworth <bill.hedworth@gmail.com>
2024-11-07 12:01:54 +0000

    remove log lib

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.bigsy/dynamo-embedded-clj "0.1.5"
+(defproject org.clojars.bigsy/dynamo-embedded-clj "0.1.7"
   :description "Embedded dynamodb for clojure"
   :url "https://github.com/Bigsy/dynamo-embedded-clj"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,6 @@
                  [integrant "0.8.0"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.clojure/tools.namespace "1.1.0"]
-                 [org.slf4j/slf4j-jdk14 "1.7.30"]
                  [net.lingala.zip4j/zip4j "1.3.2"]
                  [http-kit "2.8.0"]]
 
